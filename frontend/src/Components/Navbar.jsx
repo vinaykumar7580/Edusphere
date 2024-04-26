6f12f696898c83116877e65fae11332676256604
vinaykumar7580 <vinayhatwar16@gmail.com>
2024-04-26 14:22:32 +0530

    added react app with navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,63 @@
+import { Box, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      p={"10px 50px"}
+      boxShadow={"md"}
+      background="linear-gradient(
+        90deg,
+        rgba(2, 0, 36, 1) 0%,
+        rgba(9, 9, 121, 1) 35%,
+        rgba(0, 212, 255, 1) 100%
+      )"
+    >
+      <Box fontSize={"30px"} fontWeight={"bold"} fontFamily={"cursive"} color={"white"}>
+        <Link to={"/"}>EduSphere</Link>
+      </Box>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        gap={"50px"}
+        color={"white"}
+        fontWeight={"bold"}
+      >
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          gap={"20px"}
+        >
+          <Text _hover={{borderBottom:"3px solid blue", color:"blue"}}>
+            <Link to={"#"}>Features</Link>
+          </Text>
+          <Text _hover={{borderBottom:"3px solid blue", color:"blue"}}>
+            <Link to={"#"}>Pricing</Link>
+          </Text>
+          <Text _hover={{borderBottom:"3px solid blue", color:"blue"}}>
+            <Link to={"#"}>Community</Link>
+          </Text>
+        </Box>
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          gap={"20px"}
+        >
+          <Text _hover={{borderBottom:"3px solid blue", color:"blue"}}>
+            <Link to={"#"}>SignUp</Link>
+          </Text>
+          <Text _hover={{borderBottom:"3px solid blue", color:"blue"}}>
+            <Link to={"#"}>Login</Link>
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+export default Navbar;
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import {Box} from "@chakra-ui/react"
+import Navbar from "../Components/Navbar"
+
+
+function Home(){
+    return(
+        <Box>
+            <Navbar/>
+        </Box>
+    )
+}
+export default Home
